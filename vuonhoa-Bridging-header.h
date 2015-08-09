0fdec08cd44e564793d4945c44a786ca345b904e
conmeno <admin@MacOS-Phuong-Nguyen.local>
2015-08-09 16:48:12 +0700

    chartboott

--- vuonhoa-Bridging-header.h
+++ vuonhoa-Bridging-header.h
@@ -5,7 +5,12 @@
 //  Created by Admin on 7/22/2558 BE.
 //  Copyright (c) 2558 BE Phuong Nguyen. All rights reserved.
 //
-#import "AdTapsy.h"
+#import <Foundation/Foundation.h>
+#import <Chartboost/Chartboost.h>
+#import <AdColony/AdColony.h>
+#import <VungleSDK/VungleSDK.h>
+#import <CommonCrypto/CommonDigest.h>
+#import <AdSupport/AdSupport.h>
 #ifndef vuonhoaphatgiao_vuonhoa_Bridging_header_h
 #define vuonhoaphatgiao_vuonhoa_Bridging_header_h
 
--- vuonhoaphatgiao/ViewController.swift
+++ vuonhoaphatgiao/ViewController.swift
@@ -9,61 +9,120 @@
 import UIKit
 import iAd
 
-class ViewController: UIViewController,UIWebViewDelegate, AdTapsyDelegate  {
+
+class ViewController: UIViewController,UIWebViewDelegate, VungleSDKDelegate,  ChartboostDelegate{
     
-     //var UIiAd: ADBannerView = ADBannerView()
+     //var UIiAd:, ADBannerView = ADBannerView()
     
     @IBOutlet weak var adShow: UIButton!
     @IBOutlet weak var webView1: UIWebView!
         var WebURL = Varialbes.Static.URL
  
+    var vungleSdk = VungleSDK.sharedSDK()
+    
+    
+    //ad panel
+    var AdNumber = 1
+    @IBOutlet weak var AdView: UIView!
+    
+    @IBAction func closePanelClick(sender: AnyObject) {
+        AdView.hidden = true
+    }
+    
+    @IBAction func ShowAdClick(sender: AnyObject) {
+      
+        
+        Chartboost.showInterstitial("Home" + String(AdNumber))
+        //Chartboost.showMoreApps("Home")
+        //Chartboost.showRewardedVideo("Home")
+        vungleSdk.playAd(self, error: nil)
+        AdNumber++
+         AdColony.playVideoAdForZone("vzd576c4633e5544b8aa", withDelegate: nil)
+        if(AdNumber > 7)
+        {
+        AdView.backgroundColor = UIColor.redColor()
+        }
+        println(AdNumber)
+    }
+    
+    @IBAction func SettingClick(sender: AnyObject) {
+        AdView.hidden = false
+    }
+    
+    
+    @IBAction func MoreAppClick(sender: AnyObject) {
+    }
+    //end ad panel
+    
+    
+    
+    
       //public var NotifyURL=""
         override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib. 
-        
+            
+            
+        AdView.hidden = true
         let requestURL = NSURL(string:WebURL)
         let request = NSURLRequest(URL: requestURL!)
         webView1.loadRequest(request)
-              webView1.delegate = self;
-          //  self.canDisplayBannerAds = true
-        AdTapsy.setDelegate(self);
+        webView1.delegate = self
             
+        //show chartboost
+        Chartboost.showInterstitial("Home")
+        //show vungle 
+            vungleSdk.delegate = self
+        vungleSdk.playAd(self, error: nil)
+            //adcolony           
+        AdColony.playVideoAdForZone("vzd576c4633e5544b8aa", withDelegate: nil)
             
-        
     }
 
-//    @IBAction func TabClick(sender: UITapGestureRecognizer) {
-//        
-//        println("double click")
-//    }
-//    
+ 
+    
        override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    //vungle
+    
+    
+    // Play an ad using default settings
+    @IBAction func playAd(sender: AnyObject) {
+        var sdk = VungleSDK.sharedSDK()
+        sdk.delegate = self
+        sdk.playAd(self, error: nil)
+    }
+    func vungleSDKwillCloseAdWithViewInfo(viewInfo: [NSObject : AnyObject]!, willPresentProductSheet: Bool) {
+        println(viewInfo)
+    }
+    
+    //end vungle
+
+    
     //ad
     // Add delegate functions
-    func adtapsyDidClickedAd() {
-        println("***adtapsyDidClickedAd***");
-    }
-    
-    func adtapsyDidFailedToShowAd() {
-        println("***adtapsyDidFailedToShowAd***");
-    }
-    
-    func adtapsyDidShowAd() {
-        println("***adtapsyDidShowAd***");
-    }
-    
-    func adtapsyDidSkippedAd() {
-        println("***adtapsyDidSkippedAd***");
-    }
-    
-    func adtapsyDidCachedAd() {
-        println("***adtapsyDidCachedAd***");
-    }
+//    func adtapsyDidClickedAd() {
+//        println("***adtapsyDidClickedAd***");
+//    }
+//    
+//    func adtapsyDidFailedToShowAd() {
+//        println("***adtapsyDidFailedToShowAd***");
+//    }
+//    
+//    func adtapsyDidShowAd() {
+//        println("***adtapsyDidShowAd***");
+//    }
+//    
+//    func adtapsyDidSkippedAd() {
+//        println("***adtapsyDidSkippedAd***");
+//    }
+//    
+//    func adtapsyDidCachedAd() {
+//        println("***adtapsyDidCachedAd***");
+//    }
     //end ad
     
     //begin iad
@@ -127,27 +186,27 @@ class ViewController: UIViewController,UIWebViewDelegate, AdTapsyDelegate  {
     }
     
     func webViewDidStartLoad(webView: UIWebView) {
-        println("Webview started Loading")
+       // println("Webview started Loading")
         
         
              let currentURL : NSString = (webView1.request?.URL.absoluteString)!
         
-        println(currentURL)
+       // println(currentURL)
     }
     
     func webViewDidFinishLoad(webView: UIWebView) {
         
-        println("Webview did finish load")
+       // println("Webview did finish load")
         let currentURL : String = (webView1.request?.URL.absoluteString)!
                  println(currentURL)
         
-        if (AdTapsy.isAdReadyToShow()) {
-            println("Ad is ready to be shown");
-            AdTapsy.showInterstitial(self);
-            
-        } else {
-            println("Ad is not ready to be shown");
-        }
+//        if (AdTapsy.isAdReadyToShow()) {
+//            println("Ad is ready to be shown");
+//            AdTapsy.showInterstitial(self);
+//            
+//        } else {
+//            println("Ad is not ready to be shown");
+//        }
 
         
     }
